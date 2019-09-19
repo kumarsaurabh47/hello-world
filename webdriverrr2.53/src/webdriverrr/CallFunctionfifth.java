17dc8550b61f6b8f91991f5e1305b29c511bed44
saurabh <singhsaurabh1155@gmail.com>
2019-09-19 10:32:53 +0530

    release1.1

--- webdriverrr2.53/src/webdriverrr/CallFunctionfifth.java
+++ webdriverrr2.53/src/webdriverrr/CallFunctionfifth.java
@@ -4,10 +4,10 @@ public class CallFunctionfifth {
 
 	public static void main(String[] args) {
 		
-		FifthProgram f = new FifthProgram();
+		FifthProgram F = new FifthProgram();
 		
-		f.launchApp("http://facebook.com");
-		f.launchApp("https://support.mozilla.org");
+		F.launchApp("http://facebook.com");
+		F.launchApp("https://support.mozilla.org");
 
 	}
 
